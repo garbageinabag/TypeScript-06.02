2286cc1faa46ec8a4eae664e27372d8da0fd9726
garbageinabag <darya.mishchenko.41@gmail.com>
2024-02-06 16:05:45 +1000

    first commit

---
+++ script.ts
@@ -0,0 +1,22 @@
+class Point3D { 
+    x: number;
+    y: number;
+    z: number;
+}
+
+function abc() {
+    let point: Point = new Point();
+
+    function Move() {
+        point.x = 1; 
+        point.y = 2;
+    } 
+}
+
+let point3D: Point3D = new Point3D();
+
+function Move1() {
+    point3D.x = 1;
+    point3D.y = 2;
+    point3D.z = 3;
+}
